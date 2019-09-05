71c5b1e9242f0118f02904fe3f89bcbe8df4a4e4
Matthew Nicholson <mdn117@humboldt.edu>
2019-09-05 09:26:59 -0700

    added new xcode project. code will now build successfully, but won't run. still figuring out issue

---
+++ projects/TextEditor/src/Text Editor/Text Editor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Text Editor
+//
+//  Created by Matthew Nicholson on 9/4/19.
+//  Copyright © 2019 Matthew Nicholson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
