94559cc1d1d9f273152ea0302f2cc7f52267aa0b
Matthew Nicholson <mdn117@humboldt.edu>
2019-09-10 21:03:50 -0700

    added separate windows for each menu, including an input window. Basic typing works, and the up, down, left, right arrow keys and the enter key are implemented properly now.Currently, windows are hard-coded in their placement, but I will implement in the future resizing windows.

--- projects/TextEditor/src/Text Editor/Text Editor/TextEdit.cpp
+++ projects/TextEditor/src/Text Editor/Text Editor/TextEdit.cpp
@@ -1,12 +1,13 @@
 #define PDC_DLL_BUILD 1
 #define menu_color 1
-#define window_color 2
+#define input_window_color 2
 
 #include <curses.h>
 //#include <curspriv.h>
 #include <panel.h>
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -19,84 +20,131 @@ int main(void)
     cbreak();
     
     start_color();
-
-    int max_x = 0;
-    int max_y = 0;
-    int input_y = 3;
-    int input_x = 0;
-    WINDOW* menu_window = newwin(3, max_x, 0, 0);
-    WINDOW* input_window = newwin(max_y, max_x, input_y, input_x);
-    wmove(input_window, input_y, input_x);
-    //resize our window
-    getmaxyx(stdscr, max_y, max_x);
+    
+    int input_y = 1;
+    int input_x = 1;
+    
+    WINDOW* file_window = newwin(3, 8, 0, 0);
+    WINDOW* edit_window = newwin(3, 8, 0, 9);
+    WINDOW* search_window = newwin(3, 10, 0, 18);
+    WINDOW* view_window = newwin(3, 8, 0, 29);
+    WINDOW* options_window = newwin(3, 10, 0, 38);
+    WINDOW* help_window = newwin(3, 8, 0, 49);
+    WINDOW* info_window = newwin(3, 22, 0, 58);
+    WINDOW* input_window = newwin(20, 0, 4, 0);
+    
+    
     //window border. Integer values relate to the character used for the border lengthwise and widthwise (lines for integer 0)
+    
+    init_pair(menu_color, COLOR_BLACK, COLOR_WHITE);
+    
+    
+    wbkgd(file_window, COLOR_PAIR(menu_color));
+    wbkgd(edit_window, COLOR_PAIR(menu_color));
+    wbkgd(search_window, COLOR_PAIR(menu_color));
+    wbkgd(view_window, COLOR_PAIR(menu_color));
+    wbkgd(options_window, COLOR_PAIR(menu_color));
+    wbkgd(help_window, COLOR_PAIR(menu_color));
+    
     box(input_window, 0, 0);
-    box(menu_window, 0, 0);
+    box(edit_window, 0, 0);
+    box(file_window, 0, 0);
+    box(search_window, 0, 0);
+    box(view_window, 0, 0);
+    box(options_window, 0, 0);
+    box(help_window, 0, 0);
+    box(info_window, 0, 0);
 
     //turn on keypad input
-    keypad(stdscr, TRUE);
+    keypad(input_window, TRUE);
 
-    //hide the cursor
+    //show the cursor
     curs_set(TRUE);
 
     //MAIN PROGRAM LOGIC GOES HERE
     //add character to the screen
     
-    init_pair(menu_color, COLOR_BLACK, COLOR_WHITE);
-    
-    wbkgd(menu_window, menu_color);
+    wmove(input_window, input_y, input_x);
+    refresh();
+    wrefresh(input_window);
     
     //outputs the word File to screen
-    mvwprintw(menu_window, 1, 1, "File");
+    mvwprintw(file_window, 1, 2, "File");
     
     //outputs the word Edit to screen
-    mvwprintw(menu_window, 1, 9, "Edit");
+    mvwprintw(edit_window, 1, 2, "Edit");
     
     //outputs the word Search to screen
-    mvwprintw(menu_window, 1, 18, "Search");
+    mvwprintw(search_window, 1, 2, "Search");
     
     //outputs the word View to screen
-    mvwprintw(menu_window, 1, 29, "View");
+    mvwprintw(view_window, 1, 2, "View");
     
     //outputs the word Options to screen
-    mvwprintw(menu_window, 1, 38, "Options");
+    mvwprintw(options_window, 1, 2, "Options");
     
     //outputs the word Help to screen
-    mvwprintw(menu_window, 1, 50, "Help");
+    mvwprintw(help_window, 1, 2, "Help");
+    
+    mvwprintw(info_window, 1, 6, "Q to QUIT");
     
     
     //refresh tells curses to draw everything
     refresh();
-    wrefresh(input_window);
-    wrefresh(menu_window);
+    wrefresh(file_window);
+    wrefresh(edit_window);
+    wrefresh(search_window);
+    wrefresh(view_window);
+    wrefresh(options_window);
+    wrefresh(help_window);
+    wrefresh(info_window);
 
     //END OF PROGRAM LOGIC GOES HERE
     //Pause for user input
     int input;
-    while((input = getch()) != 'q')
+    int max_input_x = getmaxx(input_window);
+    const int max_right = 5;
+    //27 is also the number assigned to the ESC character
+    while((input = wgetch(input_window)) != 'Q')
     {
         switch (input)
         {
             case (KEY_LEFT):
                 input_x--;
+                wmove(input_window, input_y, input_x);
                 break;
             case (KEY_RIGHT):
                 input_x++;
+                wmove(input_window, input_y, input_x);
                 break;
             case (KEY_UP):
                 input_y--;
+                wmove(input_window, input_y, input_x);
                 break;
             case (KEY_DOWN):
                 input_y++;
+                wmove(input_window, input_y, input_x);
+                break;
+            case (10): //10 is the numeric value for enter in ncurses
+                input_x = 1;
+                input_y++;
+                wmove(input_window, input_y, input_x);
+                break;
+            case (max_right):
+                if (input_x == max_input_x - 1)
+                {
+                    input_x = 1;
+                    input_y++;
+                    wmove(input_window, input_y, input_x);
+                }
                 break;
             default:
                 input_x++;
                 waddch(input_window, input);
+                wmove(input_window, input_y, input_x);
                 break;
         }
-        
-        wmove(input_window, input_y, input_x);
-        refresh();
+    
         wrefresh(input_window);
     }
     
